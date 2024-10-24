a9daec5db92e1b95e92c9b938bf7f3fcf6ea3aa2
Dragon-e-T <3432968114@qq.com>
2024-10-24 13:02:10 +0800

    Lesson01 (#1)
    * 初始化项目
    
    * 初始化项目
    
    * lanShan-work
    
    * lanShan-work
    
    * lanShan-work
    
    * lanShan-work
    
    * lanShan-work

---
+++ lesson01-LV1.go
@@ -0,0 +1,18 @@
+package main
+
+//编写一个Go函数，接受两个整数作为参数，然后返回它们的和。在 main 函数中调用此函数并打印结果。
+import "fmt"
+
+func add(a int, b int) int {
+	return a + b
+}
+
+func main() {
+	var a, b int
+
+	fmt.Scan(&a)
+	fmt.Scan(&b)
+
+	sum := add(a, b)
+	fmt.Println(sum)
+}
